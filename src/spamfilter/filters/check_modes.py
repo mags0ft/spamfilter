bef30374f8365fe57ab2b2578c5b32e13b6f3f7d
mags0ft <magnusschlinsog@gmail.com>
2025-04-10 15:01:11 +0200

    add docstring and f-string to resolve pylint critical warnings

--- src/spamfilter/filters/check_modes.py
+++ src/spamfilter/filters/check_modes.py
@@ -4,8 +4,13 @@ Small utility module that checks if a mode for a given filter is applicable.
 
 
 def perform_mode_check(mode: str, possible_modes: "list[str]"):
+    """
+    Performs a check of the given `mode` string against the list of all
+    possible modes. Raises a `ValueError` if the mode is not found.
+    """
+
     if not mode in possible_modes:
         raise ValueError(
-            "Mode not accepted. This filter's mode must be one of those: %s."
-            % ", ".join(possible_modes)
+            f"Mode not accepted. This filter's mode must be one of those: \
+{', '.join(possible_modes)}."
         )
