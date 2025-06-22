116614dae6cc85889de4557f8f831b1671fc8625
mags0ft <magnusschlinsog@gmail.com>
2025-06-22 21:50:03 +0200

    add website link to top-level package docstring

--- src/spamfilter/__init__.py
+++ src/spamfilter/__init__.py
@@ -3,6 +3,8 @@ The spamfilter module is a lightweight, fast and straightforward way to build
 your own spam filtering pipelines in order to keep applications using
 user-generated content clean. It's object-oriented and makes a quick, yet
 concise approach to remove spam easy.
+
+See https://mags0ft.github.io/spamfilter/ for more details and documentation.
 """
 
 __version__ = "v2.0.0"
