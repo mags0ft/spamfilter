0fa4607ac5f9f79b24b7676aaaa081b68f89cca6
mags0ft <magnusschlinsog@gmail.com>
2025-08-30 15:56:41 +0200

    add check for missing files to JSON blocklist filter

--- src/spamfilter/filters/blocklist_from_json.py
+++ src/spamfilter/filters/blocklist_from_json.py
@@ -5,6 +5,7 @@ More information about this filter can be found in its Class docstring.
 """
 
 from json import load
+import os
 
 from .blocklist import Blocklist
 
@@ -14,11 +15,14 @@ class BlocklistFromJSON(Blocklist):
     Behaves just like the `Blocklist` class. Reads a JSON list and inserts it's
     content into the `Blocklist.blocklist` property.
 
-    - `BlocklistFromJSON.file`: filename of JSON file.
+    - `BlocklistFromJSON.file`: filename or path of the JSON file.
     """
 
     def __init__(
         self, file: str, mode: str = "normal", encoding: str = "utf-8"
     ) -> None:
+        if not os.path.isfile(file):
+            raise FileNotFoundError(f"The file '{file}' does not exist.")
+
         with open(file, "r", encoding=encoding) as f:
             super().__init__(set(load(f)), mode)
